b33adb1ef5fb6a698567c691b42de8a0194cbcd5
Mehran Abdi <mehranabdi1379@gmail.com>
2023-03-25 22:41:54 +0330

    Creating Student class and configuring it

---
+++ QuizSystem.Domain/Exceptions/StudentFirstNameRequiredException.cs
@@ -0,0 +1,14 @@
+﻿using Framework.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSystem.Domain.Exceptions
+{
+    public class StudentFirstNameRequiredException : DomainException
+    {
+        public override string Message => ExceptionMessages.StudentFirstNameRequiredExceptionMessage;
+    }
+}
--- QuizSystem.Domain/Student.cs
+++ QuizSystem.Domain/Student.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuizSystem.Domain.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,36 @@ namespace QuizSystem.Domain
 {
     public class Student
     {
-        
+        public Student()
+        {
+
+        }
+
+
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string NationalCode { get; private set; }
+        public string Password { get; private set; }
+        public DateTime BirthDate { get; private set; }
+
+        private void SetFirstName(string firstName)
+        {
+            if (string.IsNullOrEmpty(firstName))
+                throw new StudentFirstNameRequiredException();
+            FirstName = firstName;
+        }
+
+        private void SetLastName(string lastName)
+        {
+            if (string.IsNullOrEmpty(lastName))
+                throw new StudentLastNameRequiredException();
+            LastName = lastName;
+        }
+
+        private void SetNationalCode(string nationalCode)
+        {
+
+        }
     }
 }
