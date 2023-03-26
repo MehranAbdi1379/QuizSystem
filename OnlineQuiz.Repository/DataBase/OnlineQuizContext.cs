e2771b4eaada6ca9af731e9005361ad534b6e52b
MehranAbdi1379 <mehranabdi13793@gmail.com>
2023-03-26 18:26:49 +0330

    Adding Course and professor to database

--- OnlineQuiz.Repository/DataBase/OnlineQuizContext.cs
+++ OnlineQuiz.Repository/DataBase/OnlineQuizContext.cs
@@ -20,13 +20,30 @@ namespace OnlineQuiz.Repository.DataBase
         protected override void OnModelCreating(ModelBuilder model)
         {
             model.Entity<Student>().HasKey(s => s.Id);
-            model.Entity<Student>().Property(s => s.Password).IsRequired();
-            model.Entity<Student>().Property(s => s.FirstName).IsRequired();
-            model.Entity<Student>().Property(s => s.LastName).IsRequired();
-            model.Entity<Student>().Property(s => s.NationalCode).IsRequired();
+            model.Entity<Student>().Property(s => s.Id).IsRequired();
+            model.Entity<Student>().Property(s => s.Password).IsRequired().HasMaxLength(50);
+            model.Entity<Student>().Property(s => s.FirstName).IsRequired().HasMaxLength(50);
+            model.Entity<Student>().Property(s => s.LastName).IsRequired().HasMaxLength(50);
+            model.Entity<Student>().Property(s => s.NationalCode).IsRequired().HasMaxLength(10);
             model.Entity<Student>().Property(s => s.BirthDate).IsRequired();
+
+            model.Entity<Professor>().HasKey(p => p.Id);
+            model.Entity<Professor>().Property(p => p.Id).IsRequired();
+            model.Entity<Professor>().Property(p => p.Password).IsRequired().HasMaxLength(50);
+            model.Entity<Professor>().Property(p => p.FirstName).IsRequired().HasMaxLength(50);
+            model.Entity<Professor>().Property(p => p.LastName).IsRequired().HasMaxLength(50);
+            model.Entity<Professor>().Property(p => p.NationalCode).IsRequired().HasMaxLength(10);
+            model.Entity<Professor>().Property(p => p.BirthDate).IsRequired();
+
+            model.Entity<Course>().HasKey(c => c.Id);
+            model.Entity<Course>().Property(c => c.Id).IsRequired();
+            model.Entity<Course>().Property(c => c.Title).IsRequired().HasMaxLength(150);
+            model.Entity<Course>().Property(c => c.StartTime).IsRequired();
+            model.Entity<Course>().Property(c => c.EndTime).IsRequired();
         }
 
         public DbSet<Student> Students { get; set; }
+        public DbSet<Professor> Professors { get; set; }
+        public DbSet<Course> Courses{ get; set; }
     }
 }
---
+++ OnlineQuiz.Repository/Migrations/20230326145238_addingProfessorAndCourseToDB.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineQuiz.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingProfessorAndCourseToDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Students",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NationalCode",
+                table: "Students",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Students",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Students",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Accepted",
+                table: "Students",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Professors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    NationalCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Accepted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProfessorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Courses_Professors_ProfessorId",
+                        column: x => x.ProfessorId,
+                        principalTable: "Professors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CourseStudent",
+                columns: table => new
+                {
+                    CoursesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StudentsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseStudent", x => new { x.CoursesId, x.StudentsId });
+                    table.ForeignKey(
+                        name: "FK_CourseStudent_Courses_CoursesId",
+                        column: x => x.CoursesId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseStudent_Students_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_ProfessorId",
+                table: "Courses",
+                column: "ProfessorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseStudent_StudentsId",
+                table: "CourseStudent",
+                column: "StudentsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseStudent");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Professors");
+
+            migrationBuilder.DropColumn(
+                name: "Accepted",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NationalCode",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
