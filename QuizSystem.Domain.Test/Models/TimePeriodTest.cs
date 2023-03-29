1009c4666caa98a9cc623a932af1a443b308feb4
MehranAbdi1379 <mehranabdi13793@gmail.com>
2023-03-29 15:12:26 +0330

    Finishing time period class with TDD

--- QuizSystem.Domain.Test/Models/TimePeriodTest.cs
+++ QuizSystem.Domain.Test/Models/TimePeriodTest.cs
@@ -14,24 +14,39 @@ public class TimePeriodTest
     [TestMethod]
     public void SetStartDate_Retrieve()
     {
-        var startDate = DateTime.Now.AddDays(-1);
-        var timePeriod = new TimePeriod(startDate, DateTime.Now);
+        var startDate = DateTime.Now.AddDays(1);
+        var timePeriod = new TimePeriod(startDate, DateTime.Now.AddDays(15));
         Assert.AreEqual(startDate, timePeriod.StartDate);
     }
 
     [TestMethod]
     public void SetEndDate_Retrieve()
     {
-        var endDate = DateTime.Now.AddDays(1);
-        var timePeriod = new TimePeriod(DateTime.Now, endDate);
+        var endDate = DateTime.Now.AddDays(15);
+        var timePeriod = new TimePeriod(DateTime.Now.AddDays(1), endDate);
         Assert.AreEqual(endDate, timePeriod.EndDate);
     }
 
     [TestMethod]
-    public void SetStartDate_StartDateIsBeforeEndDate_ThrowException()
+    public void SetStartAndEndDate_StartDateIsBeforeEndDate_ThrowException()
     {
         var startDate = DateTime.Now;
         var endDate = DateTime.Now.AddDays(-1);
         Assert.ThrowsException<CourseEndTimeIsBeforeStartTimeException>(() => new TimePeriod(startDate, endDate));
+    }
+
+    [TestMethod]
+    public void SetStartAndEndDate_PeriodBetweenStartAndEndDateIsTooShort_ThrowException()
+    {
+        var startDate = DateTime.Now;
+        var endDate = DateTime.Now.AddDays(2);
+        Assert.ThrowsException<CourseShortPeriodException>(() => new TimePeriod(startDate, endDate));
+    }
+
+    [TestMethod]
+    public void SetStartDate_StartDateIsInTheFuture()
+    {
+        var startDate = DateTime.Now.AddDays(-3);
+        Assert.ThrowsException<CourseStartTimeInThePastException>(() => new TimePeriod(startDate, DateTime.Now.AddDays(10)));
     }
 }
--- QuizSystem.Domain/Value Object/TimePeriod.cs
+++ QuizSystem.Domain/Value Object/TimePeriod.cs
@@ -9,48 +9,39 @@ namespace QuizSystem.Domain.Value_Object
 {
     public class TimePeriod
     {
-        //public DateTime StartTime { get; set; }
-        //public DateTime EndTime { get; set; }
-
-        //public TimePeriod(DateTime startTime , DateTime endTime)
-        //{
-        //    StartTime = startTime;
-        //    EndTime = endTime;
-        //}
-
-        //public void ValidateTimePeriodLenght()
-        //{
-        //    if (EndTime.Subtract(StartTime).TotalDays < 7)
-        //        throw new CourseShortPeriodException();
-        //}
-
-        //public void ValidateStartTimeIsInTheFuture()
-        //{
-        //    if (StartTime.Subtract(DateTime.Now).TotalDays < 0)
-        //        throw new CourseStartTimeInThePastException();
-        //}
-
-        public void ValidateStartAndEndTimeSort(DateTime startDate, DateTime endDate)
-        {
-            if (endDate.Subtract(startDate).TotalDays < 0)
-                throw new CourseEndTimeIsBeforeStartTimeException();
-        }
-
-        //public void ValidateTimePeriod()
-        //{
-        //    ValidateStartAndEndTimeSort();
-        //    ValidateStartTimeIsInTheFuture();
-        //    ValidateTimePeriodLenght();
-        //}
-
         public TimePeriod(DateTime startDate, DateTime endDate)
         {
-            ValidateStartAndEndTimeSort(startDate, endDate);
+            ValidateTimePeriod(startDate, endDate);
             StartDate = startDate;
             EndDate = endDate;
         }
 
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+
+        public void ValidateTimePeriodLenght(DateTime startDate, DateTime endDate)
+        {
+            if (endDate.Subtract(startDate).TotalDays < 7)
+                throw new CourseShortPeriodException();
+        }
+
+        public void ValidateStartTimeIsInTheFuture(DateTime startDate)
+        {
+            if (startDate.Subtract(DateTime.Now).TotalDays < 0)
+                throw new CourseStartTimeInThePastException();
+        }
+
+        public void ValidateStartDateIsBeforeEndDate(DateTime startDate, DateTime endDate)
+        {
+            if (endDate.Subtract(startDate).TotalDays < 0)
+                throw new CourseEndTimeIsBeforeStartTimeException();
+        }
+
+        public void ValidateTimePeriod(DateTime startDate , DateTime endDate)
+        {
+            ValidateStartDateIsBeforeEndDate(startDate, endDate);
+            ValidateTimePeriodLenght(startDate, endDate);
+            ValidateStartTimeIsInTheFuture(startDate);
+        }
     }
 }
