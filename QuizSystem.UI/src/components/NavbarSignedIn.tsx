0d9a8d0ddbc22e5d721c8d65ee71ca592dbc571e
Mehran Abdi <mehranabdi13793@gmail.com>
2023-04-27 19:39:22 +0330

    Add signed in layout and special navbar for the page

--- QuizSystem.UI/src/components/NavbarSignedIn.tsx
+++ QuizSystem.UI/src/components/NavbarSignedIn.tsx
@@ -1,7 +1,38 @@
+import {
+  Flex,
+  Heading,
+  HStack,
+  Switch,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import React from "react";
+import NavButton from "./NavButton";
 
-const NavbarSignedIn = () => {
-  return <div>NavbarSignedIn</div>;
+interface Props {
+  name: string;
+}
+
+const NavbarSignedIn = ({ name }: Props) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Flex p={4} justifyContent="space-between">
+      <Heading>Quiz System</Heading>
+
+      <HStack spacing={3}>
+        <Text>Welcome Mr/Ms {name}</Text>
+        <NavButton to="/" name="Sign out"></NavButton>
+        <NavButton to="about-us" name="About Us"></NavButton>
+        <HStack>
+          <Switch
+            isChecked={colorMode === "dark"}
+            onChange={toggleColorMode}
+          ></Switch>
+          <Text>Dark Mode</Text>
+        </HStack>
+      </HStack>
+    </Flex>
+  );
 };
 
 export default NavbarSignedIn;
--- QuizSystem.UI/src/layouts/SignedInLayout.tsx
+++ QuizSystem.UI/src/layouts/SignedInLayout.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/NavbarRoot";
-import { Outlet } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 import NavbarSignedIn from "../components/NavbarSignedIn";
+import authApiClient from "../services/AuthApiClient";
+import GetAuthToken from "../services/Auth";
+import UserServices from "../services/UserServices";
 
 const SignedInLayout = () => {
+  const [name, setName] = useState("");
+  const params = useParams();
+  const { GetNameById } = new UserServices();
+
+  useEffect(() => {
+    if (params.id) GetNameById(params, setName);
+  }, []);
+
   return (
     <>
-      <NavbarSignedIn />
+      <NavbarSignedIn name={name}></NavbarSignedIn>
       <Outlet />
     </>
   );
