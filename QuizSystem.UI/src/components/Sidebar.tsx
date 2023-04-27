416d4363097da0d2391a52322a3090c21c5cc871
Mehran Abdi <mehranabdi13793@gmail.com>
2023-04-27 22:15:53 +0330

    Creating Admin page and adding sidebar

---
+++ QuizSystem.UI/src/components/Sidebar.tsx
@@ -0,0 +1,34 @@
+import { Box, Heading, List, ListItem, useStatStyles } from "@chakra-ui/react";
+import React, { useState } from "react";
+import CourseService from "../services/CourseService";
+import { Link, NavLink } from "react-router-dom";
+
+interface Course {
+  id: string;
+  timePeriod: {
+    endDate: Date;
+    startDate: Date;
+  };
+  title: string;
+}
+
+const AdminSidebar = () => {
+  const [courses, setCourses] = useState<Course[]>();
+  const { GetAllCourses } = new CourseService();
+  GetAllCourses(setCourses);
+
+  return (
+    <Box minHeight={{ base: "200px", md: "100vh" }}>
+      <Heading>Courses</Heading>
+      <List>
+        {courses?.map((course) => (
+          <ListItem>
+            <Link to={""}>{course.title}</Link>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+};
+
+export default AdminSidebar;
