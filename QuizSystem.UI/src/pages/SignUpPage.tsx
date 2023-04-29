b8b67b1264a0fd88f430056c95fc3e33c8b4220b
Mehran Abdi <mehranabdi13793@gmail.com>
2023-04-29 13:39:11 +0330

    Add sign up page

--- QuizSystem.UI/src/pages/SignUpPage.tsx
+++ QuizSystem.UI/src/pages/SignUpPage.tsx
@@ -9,15 +9,67 @@ import {
   Box,
   Text,
   useColorMode,
+  FormErrorMessage,
+  RadioGroup,
+  Radio,
+  HStack,
+  Flex,
 } from "@chakra-ui/react";
-import React from "react";
-import { Form } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { Form, Navigate } from "react-router-dom";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import UserServices, {
+  UserSignUp as UserSignUp,
+} from "../services/UserServices";
+import { useState } from "react";
+
+const maxDate = new Date();
+maxDate.setFullYear(maxDate.getFullYear() - 15);
+
+const schema = z.object({
+  firstName: z
+    .string()
+    .min(2, "First name should have more than 2 characters.")
+    .max(50, "First name should have less than 50 characters."),
+  lastName: z
+    .string()
+    .min(2, "Last name should have more than 2 characters.")
+    .max(50, "Last name should have less than 50 characters."),
+  nationalCode: z
+    .string()
+    .length(10, "National code should have 10 characters."),
+  password: z
+    .string()
+    .min(6, "Password should have at least 6 characters.")
+    .regex(
+      new RegExp("^(?=.*[a-zA-Z])(?=.*[0-9])"),
+      "Password should have at least a number and a character."
+    ),
+  birthDate: z.coerce
+    .date()
+    .min(new Date("1900-01-01"), "Too old...")
+    .max(maxDate, "Too young..."),
+  role: z.string(),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const SignUpPage = () => {
   const { colorMode } = useColorMode();
-  const date = new Date(1990, 1);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+  const [user, setUser] = useState<UserSignUp>();
+
+  const { SignUp } = new UserServices();
+
+  if (user) return <Navigate to="/sign"></Navigate>;
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit((data) => SignUp(data, setUser))}>
       <Container
         marginTop={5}
         bg={colorMode == "dark" ? "gray.700" : "gray.50"}
@@ -31,23 +83,56 @@ const SignUpPage = () => {
           </Box>
           <FormControl>
             <FormLabel>First name: </FormLabel>
-            <Input type="text"></Input>
+            <Input {...register("firstName")} type="text"></Input>
+            {errors.firstName && (
+              <Text color={"red.400"}>{errors.firstName.message}</Text>
+            )}
           </FormControl>
           <FormControl>
             <FormLabel>Last name: </FormLabel>
-            <Input type="text"></Input>
+            <Input {...register("lastName")} type="text"></Input>
+            {errors.lastName && (
+              <Text color={"red.400"}>{errors.lastName.message}</Text>
+            )}
           </FormControl>
           <FormControl>
             <FormLabel>National Code: </FormLabel>
-            <Input type="number"></Input>
+            <Input {...register("nationalCode")} type="number"></Input>
+            {errors.nationalCode && (
+              <Text color={"red.400"}>{errors.nationalCode.message}</Text>
+            )}
           </FormControl>
           <FormControl>
             <FormLabel>Password: </FormLabel>
-            <Input type="password"></Input>
+            <Input {...register("password")} type="password"></Input>
+            {errors.password && (
+              <Text color={"red.400"}>{errors.password.message}</Text>
+            )}
           </FormControl>
           <FormControl>
             <FormLabel>Date of birth: </FormLabel>
-            <Input type="date"></Input>
+            <Input {...register("birthDate")} type="date"></Input>
+            {errors.birthDate && (
+              <Text color={"red.400"}>{errors.birthDate.message}</Text>
+            )}
+          </FormControl>
+          <FormControl>
+            <Flex>
+              <FormLabel>Student or Professor?</FormLabel>
+              <RadioGroup>
+                <Radio {...register("role")} value="student">
+                  Student
+                </Radio>
+                <Radio {...register("role")} marginLeft={2} value="professor">
+                  Professor
+                </Radio>
+              </RadioGroup>
+            </Flex>
+            {errors.role && (
+              <Text color="red.400">
+                Please determine if you are a student or a professor.
+              </Text>
+            )}
           </FormControl>
           <FormControl>
             <Button type="submit">Submit</Button>
