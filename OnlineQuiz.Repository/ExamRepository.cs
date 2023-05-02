7f327a0acd2c510e3c0f4719bf454afb68c36de7
Mehran Abdi <mehranabdi13793@gmail.com>
2023-05-02 13:29:06 +0330

    Add exam repository

---
+++ OnlineQuiz.Repository/ExamRepository.cs
@@ -0,0 +1,30 @@
+﻿using Framework.Repository;
+using QuizSystem.Domain.Models;
+using QuizSystem.Domain.Repository;
+using QuizSystem.Repository.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSystem.Repository
+{
+    public class ExamRepository : BaseRepository<Exam, ApiUser>, IExamRepository
+    {
+        public ExamRepository(QuizSystemContext context) : base(context)
+        {
+
+        }
+
+        public bool ExamTitleExists(string title, Guid courseId)
+        {
+            return context.Set<Exam>().Where(s => s.CourseId == courseId).Any(s => s.Title == title);
+        }
+
+        public List<Exam> GetAllExams(Guid courseId)
+        {
+            return context.Set<Exam>().Where(x => x.CourseId == courseId).ToList();
+        }
+    }
+}
---
+++ QuizSystem.Domain/Repository/IExamRepository.cs
@@ -0,0 +1,11 @@
+﻿using Framework.Repository;
+using QuizSystem.Domain.Models;
+
+namespace QuizSystem.Domain.Repository
+{
+    public interface IExamRepository : IBaseRepository<Exam>
+    {
+        bool ExamTitleExists(string title, Guid courseId);
+        List<Exam> GetAllExams(Guid courseId);
+    }
+}
