0b53cf7e4e9040bb150676dcef6d19b42cb50a71
Mehran Abdi <mehranabdi13793@gmail.com>
2023-05-04 11:28:24 +0330

    Add multiple choice question repository

---
+++ OnlineQuiz.Repository/MultipleChoiceQuestionRepository.cs
@@ -0,0 +1,29 @@
+﻿using Framework.Repository;
+using QuizSystem.Domain.Models;
+using QuizSystem.Repository.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSystem.Repository
+{
+    public class MultipleChoiceQuestionRepository : BaseRepository<MultipleChoiceQuestion, ApiUser>, IMultipleChoiceQuestionRepository
+    {
+        public MultipleChoiceQuestionRepository(QuizSystemContext context) : base(context)
+        {
+
+        }
+
+        public bool TitleExist(string title , Guid professorId , Guid courseId)
+        {
+            return context.Set<MultipleChoiceQuestion>().Any(x => x.Title == title && x.ProfessorId == professorId && x.CourseId ==courseId);
+        }
+
+        public List<MultipleChoiceQuestion> GetWithCourseAndProfessorId(Guid courseId, Guid professorId)
+        {
+            return context.Set<MultipleChoiceQuestion>().Where(x => x.CourseId == courseId && x.ProfessorId == professorId).ToList();
+        }
+    }
+}
