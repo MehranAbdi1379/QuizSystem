3853a5fa3a098d3b5aef3244b48420844e2f658b
Mehran Abdi <mehranabdi13793@gmail.com>
2023-05-04 11:27:59 +0330

    Add graded question repository

---
+++ OnlineQuiz.Repository/GradedQuestionRepository.cs
@@ -0,0 +1,23 @@
+﻿using Framework.Repository;
+using QuizSystem.Domain.Models;
+using QuizSystem.Repository.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSystem.Repository
+{
+    public class GradedQuestionRepository : BaseRepository<GradedQuestion, ApiUser>, IGradedQuestionRepository
+    {
+        public GradedQuestionRepository(QuizSystemContext context) : base(context)
+        {
+        }
+
+        public bool QuestionIsDuplicate(Guid questionId, Guid examId)
+        {
+            return context.Set<GradedQuestion>().Any(x => x.ExamId == examId && x.QuestionId == questionId);
+        }
+    }
+}
