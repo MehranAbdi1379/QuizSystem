ce7aa1c0bc0c8d4c18fd92c68f5324301e33d5bb
Mehran Abdi <mehranabdi13793@gmail.com>
2023-05-28 07:29:34 +0330

    Add exam student DTOs

---
+++ OnlineQuiz.Service.Contracts/DTO/ExamStudentUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSystem.Service.Contracts.DTO
+{
+    public class ExamStudentAddGradeDTO
+    {
+        public Guid Id { get; set; }
+        public double Grade { get; set; }
+    }
+}
---
+++ OnlineQuiz.Service.Contracts/IStudentExamService.cs
@@ -0,0 +1,14 @@
+﻿using QuizSystem.Domain.Models;
+using QuizSystem.Service.Contracts.DTO;
+
+namespace QuizSystem.Service
+{
+    public interface IExamStudentService
+    {
+        ExamStudent CountDownTimeLeft(IdDTO dto);
+        ExamStudent Create(ExamStudentCreateDTO dto);
+        void Delete(IdDTO dto);
+        ExamStudent FinishExam(IdDTO dto);
+        ExamStudent UpdateGrade(ExamStudentAddGradeDTO dto);
+    }
+}
