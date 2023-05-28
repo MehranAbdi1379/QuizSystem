8ec8ce8ee454d766b5195ef572978c6af3bd4788
Mehran Abdi <mehranabdi13793@gmail.com>
2023-05-28 07:28:55 +0330

    Add exam student repository

---
+++ OnlineQuiz.Repository/ExamStudentRepository.cs
@@ -0,0 +1,19 @@
+﻿using Framework.Repository;
+using QuizSystem.Domain.Models;
+using QuizSystem.Domain.Repository;
+using QuizSystem.Repository.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSystem.Repository
+{
+    public class ExamStudentRepository : BaseRepository<ExamStudent, ApiUser> , IExamStudentRepository
+    {
+        public ExamStudentRepository(QuizSystemContext context) : base(context)
+        {
+        }
+    }
+}
