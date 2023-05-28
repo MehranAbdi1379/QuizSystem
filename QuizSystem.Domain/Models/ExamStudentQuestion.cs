2f97d518d2017f39d0be53bcd6aaec2adaaa19da
Mehran Abdi <mehranabdi13793@gmail.com>
2023-05-28 07:30:28 +0330

    Add exam student question model

---
+++ QuizSystem.Domain/Models/ExamStudentQuestion.cs
@@ -0,0 +1,44 @@
+﻿using Framework.Core.Domain;
+using QuizSystem.Domain.Repository;
+using QuizSystem.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSystem.Domain.Models
+{
+    public class ExamStudentQuestion: BaseEntity
+    {
+        public ExamStudentQuestion()
+        {
+
+        }
+
+        public ExamStudentQuestion(Guid examStudentId , Guid gradedQuestionId , string answer , IExamStudentRepository examStudentRepository, IGradedQuestionRepository gradedQuestionRepository)
+        {
+            SetExamStudentId(examStudentId , examStudentRepository);
+            SetGradedQuestionId(gradedQuestionId, gradedQuestionRepository);
+            Answer = answer;
+        }
+
+        public Guid ExamStudentId { get; private set; }
+        public Guid GradedQuestionId { get; private set; }
+        public string Answer { get; set; } 
+
+        public void SetExamStudentId(Guid examStudentId , IExamStudentRepository examStudentRepository)
+        {
+            if (!examStudentRepository.IsExist(examStudentId))
+                throw new Exception();
+            ExamStudentId = examStudentId;
+        }
+
+        public void SetGradedQuestionId(Guid gradedQuestionId , IGradedQuestionRepository gradedQuestionRepository)
+        {
+            if (!gradedQuestionRepository.IsExist(gradedQuestionId))
+                throw new Exception();
+            GradedQuestionId = gradedQuestionId;
+        }
+    }
+}
---
+++ QuizSystem.Domain/Repository/IExamStudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Framework.Repository;
+using QuizSystem.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSystem.Domain.Repository
+{
+    public interface IExamStudentRepository: IBaseRepository<ExamStudent>
+    {
+    }
+}
