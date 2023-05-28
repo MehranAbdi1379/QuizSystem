f0606e3bff0734e53a4b4fa960fc682133c82e7b
Mehran Abdi <mehranabdi13793@gmail.com>
2023-05-28 09:07:08 +0330

    Add exam student question repository

---
+++ OnlineQuiz.Repository/ExamStudentQuestionRepository.cs
@@ -0,0 +1,28 @@
+﻿using Framework.Repository;
+using QuizSystem.Domain.Models;
+using QuizSystem.Repository.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSystem.Repository
+{
+    public class ExamStudentQuestionRepository : BaseRepository<ExamStudentQuestion, ApiUser>, IExamStudentQuestionRepository
+    {
+        public ExamStudentQuestionRepository(QuizSystemContext context) : base(context)
+        {
+        }
+
+        public bool ExamStudentQuestionAlreadyExist(Guid examStudentId, Guid gradedQuestionId)
+        {
+            return context.Set<ExamStudentQuestion>().Any(x => x.ExamStudentId == examStudentId && x.GradedQuestionId == gradedQuestionId);
+        }
+
+        public ExamStudentQuestion GetWithExamStudentAndGradedQuestionId(Guid examStudentId, Guid gradedQuestionId)
+        {
+            return context.Set<ExamStudentQuestion>().Where(x => x.GradedQuestionId == gradedQuestionId && x.ExamStudentId == examStudentId).First();
+        }
+    }
+}
