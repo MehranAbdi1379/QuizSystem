40fd809a34609caa4aa80046c705030dcf4b0e89
Mehran Abdi <mehranabdi13793@gmail.com>
2023-05-29 19:23:39 +0330

    Add exam student services to frontend

---
+++ QuizSystem.UI/src/services/ExamStudentService.ts
@@ -0,0 +1,38 @@
+import authApiClient from "./AuthApiClient";
+
+class ExamStudentService{
+    Create(examId: any , setExamStudent: any,setError: any)
+    {
+        authApiClient().post('Exam/ExamStudent/Create' , {examId , studentId: localStorage.getItem('userId')}).then(res => setExamStudent(res.data))
+        .catch(err => setError(err.response.data))
+    }
+    GetByExamAndStudentId(examId: any , setExamStudent: any,setError: any)
+    {
+        authApiClient().post('Exam/ExamStudent/Get' , {examId, studentId: localStorage.getItem('userId')}).then(res => setExamStudent(res.data))
+        .catch(err => setError(err.response.data))
+    }
+    Exist(examId:any , setExist: any , setError: any)
+    {
+        authApiClient().post('Exam/ExamStudent/Exist' , {examId , studentId: localStorage.getItem('userId')}).then(res => setExist(res.data)).catch(err => setError(err.response.data))
+    }
+    CountDownTimeLift(examStudentId: any , setError: any)
+    {
+        authApiClient().post('Exam/ExamStudent/UpdateTimeLeft', {id: examStudentId}).catch(err => setError(err.response.data))
+    }
+    CreateQuestion(examStudentId: any , gradedQuestionId: any , answer: any ,setExamStudentQuestion: any, setError:any)
+    {
+        authApiClient().post('Question/ExamStudentQuestion/Create' , {examStudentId, gradedQuestionId,answer})
+        .then(res => setExamStudentQuestion(res.data)).catch(err => setError(err.response.data))
+    }
+    GetQuestion(gradedQuestionId: any , examStudentId: any , setExamStudentQuestion: any, setError:any)
+    {
+        authApiClient().post('Question/ExamStudentQuestion/Get' , {gradedQuestionId, examStudentId})
+        .then(res => setExamStudentQuestion(res.data)).catch(err => setError(err.response.data))
+    }
+    UpdateQuestion(id: any , answer: any , setError: any)
+    {
+        authApiClient().patch('Question/ExamStudentQuestion/Update', {id, answer}).catch(err => setError(err.response.data))
+    }
+}
+
+export default ExamStudentService
