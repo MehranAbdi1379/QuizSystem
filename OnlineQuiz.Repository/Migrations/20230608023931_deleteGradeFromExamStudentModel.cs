5ad877834d438d6214120e18bf58eb047184b779
Mehran Abdi <mehranabdi13793@gmail.com>
2023-06-08 06:34:08 +0330

    Delete grade from examStudent model

---
+++ OnlineQuiz.Repository/Migrations/20230608023931_deleteGradeFromExamStudentModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizSystem.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class deleteGradeFromExamStudentModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Grade",
+                table: "ExamStudent");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Grade",
+                table: "ExamStudent",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
