25c21a38c7eefc90521a68f61b7da0efb1bae6d7
Mehran Abdi <mehranabdi13793@gmail.com>
2023-10-12 13:37:47 +0330

    add descriptive question seed data

---
+++ OnlineQuiz.Repository/SeedData/DescriptiveQuestionSeedData.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using QuizSystem.Domain.Models;
+using QuizSystem.Repository.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizSystem.Repository.SeedData
+{
+    public static class DescriptiveQuestionSeedData
+    {
+        public async static Task SeedData(QuizSystemContext context)
+        {
+            var courses = context.Set<Course>().ToList().Select(x => new { x.ProfessorId, x.Id});
+
+            var facker = new Faker<DescriptiveQuestion>()
+                .RuleFor(e => e.Description, f => f.Lorem.Sentence())
+                .RuleFor(e => e.Title, f => f.Random.Words(2))
+                .RuleFor(e => e.ProfessorId, f => f.PickRandom(courses.Select(c => c.ProfessorId)))
+                .RuleFor(e => e.CourseId , f => f.PickRandom(courses.Select(c => c.Id)));
+
+            var dummyDescriptiveQuestions = facker.Generate(200);
+
+            foreach (var descriptiveQuestion in dummyDescriptiveQuestions)
+            {
+                context.Set<DescriptiveQuestion>().Add(descriptiveQuestion);
+            }
+            context.SaveChanges();
+        }
+    }
+}
